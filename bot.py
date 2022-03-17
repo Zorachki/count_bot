e4471c6f2bfee6cff50aec9d9ffdebb8cd2101d5
Zorachki <77637975+Zorachki@users.noreply.github.com>
2022-03-17 11:30:43 +0300

    Add reset_time to put in and change the language

--- bot.py
+++ bot.py
@@ -3,10 +3,10 @@ import schedule
 import time
 from threading import Thread
 
-### Вводные данные (токен, участники, время сброса)
+### Input data (token, participants, reset time)
 bot = telebot.TeleBot('1373754699:AAE2ZCO8kiUQ7ZT3XQqWxzSBNsqkwFg82Os')
 employees = ['Zorachki', 'lesja_hick', 'frolkin_test',]
-reset_time = "22:10"    # в формате HH:MM(:SS)
+reset_time = "22:10"    # in HH:MM(:SS) format
 
 sleeping_list = employees[:]
 woke_up_list = []
@@ -14,37 +14,35 @@ woke_up_list = []
 @bot.message_handler(content_types='text')
 def get_text_messages(message):
     username = message.from_user.username
-    ### Выводит список участников в столбик
+    ### Show the list of participants in a column 
     delimiter = '\n'
     awakened = delimiter.join(woke_up_list)
     sleeping = delimiter.join(sleeping_list)
     if message.text == '/status':
         if len(woke_up_list) == 0:
-            bot.send_message(message.chat.id, "Все ещё спят")
+            bot.send_message(message.chat.id, "No one woke up")
         elif len(sleeping_list) == 0:
-            bot.send_message(message.chat.id, f"Все проснулись:\n{awakened}")
-        else: bot.send_message(message.chat.id, f'Проснулись:\n{awakened}\n\nЕщё спят:\n{sleeping}')
+            bot.send_message(message.chat.id, f"Everyone woke up:\n{awakened}")
+        else: bot.send_message(message.chat.id, f'Woke up:\n{awakened}\n\nStill sleeping:\n{sleeping}')
     elif message.text == '/help':
-        bot.send_message(message.chat.id, '/help — помощь\n/status — проснувшиеся\n/count — количество проснувшихся')
+        bot.send_message(message.chat.id, '/help — help\n/status — awakened\n/count — number of awakened')
     elif message.text == '/count':
         current_count = len(woke_up_list)
         total_count = len(employees)
-        bot.send_message(message.chat.id, f'Проснулось {current_count} из {total_count}')
-    if message.text == "loh":
-        bot.send_message(message.chat.id, "Лох")
-    ### Добавляет человека в список проснувшихся и удаляет из спящих
+        bot.send_message(message.chat.id, f'Woke up {current_count} out of {total_count}')
+    ### Adds a participant to the woke up list and removes from the sleepers
     if username in sleeping_list:
         woke_up_list.append(username)
         sleeping_list.remove(username)
    
 def list_reset():
-    """Переносит всех из списка проснувшихся в ещё спящих"""
+    """Transfers everyone from the woke up list to still sleeping"""
     for woke in woke_up_list:
         sleeping_list.append(woke)
     woke_up_list.clear()
 
 def repeat():
-    """Запускает сброс списков каждый день в заданное время"""
+    """Run resetting lists every day at the specified time"""
     schedule.every().day.at(reset_time).do(list_reset)
     while True:
         schedule.run_pending()
